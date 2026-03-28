a80f8124381b3e219c3643e03c2ad3792e613ac4
tpfm0708hub <tpfm0708@gmail.com>
2026-03-28 14:27:52 +0900

    260328_df생성.py
    Added comments to clarify the purpose of the code and the data being generated.

--- 260328_df생성.py
+++ 260328_df생성.py
@@ -1,13 +1,20 @@
 import pandas as pd
 import numpy as np
 
-idx_01=list(range(1,500+1))
+#  임의의 표본 500개 생성
+idx_01=list(
+    range(1,500+1))
+
+#  주소구성: "수정구", "분당구", "중원구"
 address_01=np.random.choice(
     ["수정구","분당구","중원구"],size=len(idx_01),replace=True
     )
+
+#  수입범위: 240~500
 income_01=np.random.choice(
     range(240,500+1),size=len(idx_01),replace=True
     )
+
 df_01=pd.DataFrame({
     '순번':idx_01,
     '주소':address_01,
