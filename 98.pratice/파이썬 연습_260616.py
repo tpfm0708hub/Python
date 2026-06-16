3c25b9ea1c9ba333b8563631ccc12b2ab5350f8a
SEHYEON JANG <tpfm0708@gmail.com>
2026-06-16 19:38:56 +0900

    260616_파이썬 연습
    'KNHANES_chronic_disease_260616_diabetes_health_behavior_drink_amt.R'파일 내용을 바탕으로
    분석 파일 일괄적 불러오기 및 데이터 전처리 연습을 하였습니

---
+++ 98.pratice/파이썬 연습_260616.py
@@ -0,0 +1,64 @@
+import os
+import multiprocessing
+import pandas as pd
+import numpy as np
+from pathlib import Path
+from joblib import Parallel, delayed
+
+cl = multiprocessing.cpu_count() - 1
+
+path_001 = r'D:/github'
+
+df_list_001 = list(Path(path_001).glob('hn*.sas7bdat'))
+
+
+def read_sas_001(file_01):
+    return pd.read_sas(file_01)
+
+df_ls_001 = Parallel(n_jobs = -1)(delayed(read_sas_001)(file_02) for file_02 in df_list_001)
+
+df_001 = pd.concat(df_ls_001, ignore_index = True, axis = 0)
+
+df_001 = df_001[df_001['age'] >= 20]
+
+
+df_002 = df_001.copy()
+df_002['age_group'] = np.select([
+    (df_002['age'] >= 80),
+    (df_002['age'] >= 60),
+    (df_002['age'] >= 40),
+    (df_002['age'] >= 20),
+    ], [3, 2, 1, 0], default = 99)
+
+df_003 = df_002[df_002['HE_ht'].notna() & df_002['HE_wt'].notna()]
+
+df_003['set_bmi'] = df_003['HE_wt'] / (df_003['HE_ht'] / 100)**2
+
+df_004 = df_003[(df_003['BS1_1'] != 9) & (df_003['BS12_37'] != 9) &
+                df_003['BS1_1'].notna() & df_003['BS12_37'].notna()
+                ]
+
+df_003['current_smoking'] = np.select([
+    (df_003['BS3_1'].isin([1,2]))|(df_003['BS12_47'].isin([1,2])),
+    (df_003['BS3_1'] ==3)         |(df_003['BS12_47'] == 3),
+    (df_003['BS3_1'] ==8)         |(df_003['BS12_47'] == 8)
+    ], [2, 1, 0])
+
+df_005 = df_004[df_004['BD1_11'] != 9]
+
+df_006 = df_005.rename(columns = {'current_smoking': 'smoke_current'})
+df_006['smoke_amt'] = df_006[['BS3_2', 'BS12_47_1']].sum(axis = 1)
+
+print(df_006['BD2_1'].value_counts(dropna=False).sort_index())
+
+df_006['BD2_1'] = np.where(df_006['BD2_1'] == 8, 0, df_006['BD2_1'])
+
+df_007 = df_006.copy()
+df_007 = df_007.rename(columns = {'current_drinking': 'drink_current'})
+df_007['drink_amt'] = np.select([
+    (df_007['BD2_1']== 5),(df_007['BD2_1']== 4),
+    (df_007['BD2_1']== 3),(df_007['BD2_1']== 2),
+    (df_007['BD2_1']== 1)
+    ], [df_007['BD2_14'], 8.0, 5.5, 3.5, 1.5])
+
+print(df_007['drink_amt'].describe())
